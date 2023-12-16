886cfb98970143aa3d8f970e2bcd0db034b321da
kazu107 <kazu.uttu107@gmail.com>
2023-12-16 22:47:22 +0900

    配列回転

--- rotate90.cpp
+++ rotate90.cpp
@@ -17,7 +17,7 @@ void Rotate90(vector<string> &vec) {//string ver.
 
 void Rotate90(vector<vector<int>> &vec) { //int ver.
     int x = (int)vec.size(), y = (int)vec[0].size();
-    vector<string> s(y, vector<int>(x));
+    vector<vector<int>> s(y, vector<int>(x));
     for (int i = 0; i < x; i ++) {
         for (int j = 0; j < y; j ++) {
             s[y - j - 1][i] = vec[i][j];
