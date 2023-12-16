93ba1f9c2c9b4f7db3ede88b39821a9ce7276eaf
kazu107 <kazu.uttu107@gmail.com>
2023-12-16 22:53:10 +0900

    部分木の大きさ数えるやつ

--- Subtreesize.cpp
+++ Subtreesize.cpp
@@ -1,4 +1,4 @@
-//ある根の部分木の大きさを数える
+//ある根の部分木の大きさを数える・
 //tr(根, 部分木, 頂点の数のbool配列(falseで初期化))
 int tr(int x, vector<vector<int>> &vec, vector<bool> &check) {
     if ((int)vec[x].size() <= 1) return 1;
