d58b010b1a0625936860da9606c2f43386b4f2f1
kazu107 <kazu.uttu107@gmail.com>
2024-03-28 01:36:53 +0900

    累積和

--- Rangesum.cpp
+++ Rangesum.cpp
@@ -1,9 +1,13 @@
 /*
 Rangesum(累積和をとった配列, 範囲のはじめ, 終わり)
 0 <= l, r <= len - 1
+制約を守ってない場合は0を返す
 */
 ll Rangesum(vector<ll> &vec, int l, int r) {
     ll ansL, ansR;
+    if (l > r) swap(l, r);
+    if (l < 0) return 0;
+    if (r >= (int)vec.size()) return 0;
     if (l == 0) ansL = 0;
     else ansL = vec[l - 1];
     ansR = vec[r];
